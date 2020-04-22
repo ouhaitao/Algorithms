1437e9b6c7961516ed4854a4daa254e52362b5c8
godbless <parry@thecover.co>
2020-04-22 21:11:40 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,35 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println(fun(7));
+        System.out.println(fun1(18));
     }
     
+    /**
+     * 4n+7m
+     */
+    private static boolean fun1(int n) {
+        if (n % 4 == 0) {
+            return true;
+        }
+        if (n % 7 == 0) {
+            return true;
+        }
+        for (int i = 7; i < n; i+=7) {
+            if ((n-i)%4 == 0) {
+                System.out.println(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
     public static int fun(int n) {
-        int[] f = new int[n+1];
+        int[] f = new int[n + 1];
         f[1] = 1;
         f[2] = 2;
         for (int i = 3; i <= n; i++) {
-            f[i] = f[i-1] + f[i-2];
+            f[i] = f[i - 1] + f[i - 2];
         }
         return f[n];
     }
--- src/排序/快速排序.java
+++ src/排序/快速排序.java
@@ -3,26 +3,28 @@ package 排序;
 import java.util.Arrays;
 
 public class 快速排序 {
-
+    
     public static void main(String[] args) {
-
+        
         int[] data = {6, 2, 7, 3, 8, 9};
-        Arrays.stream(quickSort(data, 0, data.length - 1)).forEach(x -> System.out.print(x + " "));
+        int[] data1 = {1, 2, 3, 4, 5, 6};
+        int[] data2 = {6, 5, 4, 3, 2, 1};
+        Arrays.stream(quickSort1(data2, 0, data2.length - 1)).forEach(x -> System.out.print(x + " "));
     }
-
+    
     /**
      * 递归方式的快排,时间复杂度为O(n)
-     * @param data 待排数组
+     *
+     * @param data  待排数组
      * @param start 待排元素起始下标
-     * @param end 待排元素结束下标
-     * @return
+     * @param end   待排元素结束下标
      */
-
+    
     public static int[] quickSort(int[] data, int start, int end) {
         if (end <= start) {
             return data;
         }
-
+        
         //简便起见,确定枢纽元,这里以下标为start的元素
         int key = data[start];
         int i = 0;
@@ -45,5 +47,39 @@ public class 快速排序 {
         //对枢纽元右边的元素进行排序
         data = quickSort(data, start + i + 1, end);
         return data;
+    }
+    
+    /**
+     * 原地排序
+     */
+    public static int[] quickSort1(int[] data, int start, int end) {
+        if (end <= start) {
+            return data;
+        }
+        int key = partition(data, start, end);
+        quickSort1(data, start, key - 1);
+        quickSort1(data, key + 1, end);
+        return data;
+    }
+    
+    /**
+     * 返回枢纽元的下标
+     */
+    public static int partition(int[] data, int start, int end) {
+        int key = data[end];
+//        [start,i)是小于枢纽元的区间,(i,end]是大于等于枢纽元的区间
+        int i = start;
+        for (int j = start; j < end; j++) {
+            if (data[j] < key) {
+                int tmp = data[i];
+                data[i] = data[j];
+                data[j] = tmp;
+                i++;
+            }
+        }
+        int tmp = data[i];
+        data[i] = data[end];
+        data[end] = tmp;
+        return i;
     }
 }
